6adc042b43fdf59c7d059a2f4d0adcbee75fcf5f
Eduardo Nascimento <edwardo.xavier@gmail.com>
2022-03-18 19:58:22 -0300

    Update of code

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const pokedex = [
+
+  {
+    id: 1,
+    nome: "Bulbassur",
+    descricao: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowiy grows larger.",
+    tipo: "Grass",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+
+  },
+  {
+    id: 2,
+    nome: "Charmander",
+    descricao: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    tipo: "Fire",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  },
+  {
+    id: 3,
+    nome: "Squirtle",
+    descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    tipo: "Water",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+  }
+]
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  res.render("index", pokedex);
+});
+
+app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
